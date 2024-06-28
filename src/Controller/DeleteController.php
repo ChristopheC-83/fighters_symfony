bdb43c50cf974928425334df15c2364f1144e756
kikisan <kiketdule@gmail.com>
2024-06-28 18:31:50 +0200

    suppression personnage ok

---
+++ src/Controller/DeleteController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Fighters;
+use App\Repository\FightersRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DeleteController extends AbstractController
+{
+    #[Route('/delete/{id}', name: 'app_delete')]
+    public function index($id, FightersRepository $fightersRepository,  EntityManagerInterface $entityManager, Request $request): Response
+    {
+        //  Le combattant existe il ?
+        $fighter = $fightersRepository->findOneById($id);
+        if(!$fighter){
+            $this->addFlash('warning', "Ce personnage semble ne pas exister !");
+            return $this->redirectToRoute('app_home');
+        }
+        // dd($fighter);
+
+        // Si oui, on récupère son image 
+        $imageName = $fighter->getImage();
+
+        // Supprime le fighter de la base de données
+        $entityManager->remove($fighter);
+        $entityManager->flush();
+
+        // Supprime le fichier de l'image si elle existe
+        if ($imageName) {
+            $filesystem = new Filesystem();
+            $filesystem->remove($this->getParameter('images_fighters_directory') . '/' . $imageName);
+        }
+
+        $this->addFlash('success', 'Le fighter a été supprimé avec succès.');
+        return $this->redirectToRoute('app_home');
+    }
+
+        
+    }
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FightersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FightersRepository $fightersRepository): Response
+
     {
-        
-        return $this->render('home/index.html.twig');
+        // dd($fightersRepository->findAll());
+        return $this->render('home/index.html.twig', [
+            'fighters' => $fightersRepository->findAll(),
+        ]);
     }
 }
